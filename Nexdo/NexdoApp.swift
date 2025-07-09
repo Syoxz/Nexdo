8645cb7937a7dc66aea172941dbf60cd5e3791e8
syoxz <marcelgast240@hotmail.com>
2025-07-09 19:29:08 +0200

    Initial Commit

---
+++ Nexdo/NexdoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NexdoApp.swift
+//  Nexdo
+//
+//  Created by Marcel Gast on 09.07.25.
+//
+
+import SwiftUI
+
+@main
+struct NexdoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
